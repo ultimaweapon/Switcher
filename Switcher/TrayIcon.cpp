07a32fe6071962182a8a8266307ba9bcce8e118b
Putta Khunchalee <ultimaweapon@outlook.com>
2016-09-26 18:57:40 +0700

    Use notify icon version 4

--- Switcher/TrayIcon.cpp
+++ Switcher/TrayIcon.cpp
@@ -8,6 +8,7 @@ CTrayIcon::CTrayIcon(CWindow& owner, REFGUID id) : m_owner(owner)
 	m_iconData.cbSize = sizeof(m_iconData);
 	m_iconData.hWnd = owner;
 	m_iconData.uFlags = NIF_GUID;
+	m_iconData.uVersion = NOTIFYICON_VERSION_4;
 	m_iconData.guidItem = id;
 }
 
@@ -15,13 +16,26 @@ VOID CTrayIcon::Create(_U_STRINGorID icon, PCWSTR pszTip, UINT uMessageId)
 {
 	ATLASSERT(pszTip);
 
-	m_iconData.uFlags |= NIF_MESSAGE | NIF_ICON | NIF_TIP;
+	// Add icon.
+	m_iconData.uFlags |= NIF_MESSAGE | NIF_ICON | NIF_TIP | NIF_SHOWTIP;
 	m_iconData.uCallbackMessage = uMessageId;
 	m_iconData.hIcon = AtlLoadIconImage(icon);
 	wcscpy_s(m_iconData.szTip, pszTip);
 
 	if (!Shell_NotifyIcon(NIM_ADD, &m_iconData))
 		AtlThrow(E_UNEXPECTED);
+
+	// Change version to 4.
+	try
+	{
+		if (!Shell_NotifyIcon(NIM_SETVERSION, &m_iconData))
+			AtlThrow(E_UNEXPECTED);
+	}
+	catch (...)
+	{
+		ATLTRY(Destroy());
+		throw;
+	}
 }
 
 VOID CTrayIcon::Destroy()
